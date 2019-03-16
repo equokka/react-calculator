8eebc3b25998cb09b84eae35de87ba1f415b3d4e
Daniel <daniel.padez@gmail.com>
2019-03-16 11:48:58 +0000

    builtd

---
+++ precache-manifest.4e68708b043c4c0cca5426715cf00041.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e546b3e323215319a407",
+    "url": "/react-calculator/static/js/runtime~main.00b8a6fe.js"
+  },
+  {
+    "revision": "c2fce9279637d0820591",
+    "url": "/react-calculator/static/js/main.5c86b631.chunk.js"
+  },
+  {
+    "revision": "61aac096e52981e52e26",
+    "url": "/react-calculator/static/js/2.b1c32fee.chunk.js"
+  },
+  {
+    "revision": "c2fce9279637d0820591",
+    "url": "/react-calculator/static/css/main.15b7289c.chunk.css"
+  },
+  {
+    "revision": "5b46007097a34e1e932d53519c233ced",
+    "url": "/react-calculator/index.html"
+  }
+];
